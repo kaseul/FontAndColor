ca76eb21f3b99454be23703f4fa25c698bfd5c60
kaseul <s2017s02@e-mirim.hs.kr>
2019-09-23 09:33:48 +0900

    #3 코드 구현 by 3502 김가슬

--- FontAndColor/Form1.cs
+++ FontAndColor/Form1.cs
@@ -15,5 +15,36 @@ namespace FontAndColor
         {
             InitializeComponent();
         }
+
+        private void TsbtnFont_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.fontDlg.ShowDialog();
+
+            switch(result)
+            {
+                case DialogResult.OK:
+                    // this.rtbText.Font = this.fontDlg.Font; // 전체 글자 폰트 바꾸기
+                    this.rtbText.SelectionFont = this.fontDlg.Font; // 사용자가 드래그 한 부분만 폰트 바꾸기
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("폰트 변경을 취소했어요", "알림");
+                    break;
+            }
+        }
+
+        private void TsbtnColor_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.colorDlg.ShowDialog();
+            
+            switch(result)
+            {
+                case DialogResult.OK:
+                    this.rtbText.SelectionColor = this.colorDlg.Color;
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("색 변경을 취소했네요", "알림");
+                    break;
+            }
+        }
     }
 }
